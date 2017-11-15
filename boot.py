18c28c4a37090968c431bb935064b965e07fc4f0
jaredcrean <jaredacrean@gmail.com>
2017-11-14 22:53:00 -0700

    updated a few things

--- boot.py
+++ boot.py
@@ -5,7 +5,7 @@ sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
     print('connecting to network...')
     sta_if.active(True)
-    sta_if.connect('wifi-SSID', 'Password')
+    sta_if.connect('wifi-access-point-name', 'password')
     while not sta_if.isconnected():
         pass
 print('network config:', sta_if.ifconfig())
--- main.py
+++ main.py
@@ -2,27 +2,31 @@ import socket
 import machine
 import webrepl
 import time
+import network
 
-webrepl.start()
+#webrepl.start()
 
 #HTML to send to browsers
 html = """<!DOCTYPE html>
 <html>
-<head> <title>ESP8266 Garage Door  </title> </head>
+<head> <title>Wifi Garage Door Opener</title> </head>
 <center><h2>Garage Door Opener</h2></center>
 <form>
-GD0:
-<button name="Garage Toggle" value="OFF0" type="submit">Open Garage Door</button><br><br>
+OPEN/CLOSE:
+<button name="Open-Close" value="OFF0" type="submit">Open Garage Door</button><br><br>
 </form>
 </html>
 """
 
 #Setup PINS
-GD0 = machine.Pin(5, machine.Pin.OUT)
+PIN1 = machine.Pin(5, machine.Pin.OUT)
+
+#sta_if = network.WLAN(network.STA_IF)
+
 
 #Setup Socket WebServer
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
+s.bind(('192.168.1.240', 80))
 s.listen(5)
 while True:
     conn, addr = s.accept()
@@ -30,10 +34,10 @@ while True:
     request = conn.recv(1024)
     print("Content = %s" % str(request))
     request = str(request)
-    GDOFF0 = request.find('/?GD=OFF0')
-    if GDOFF0 == 6:
-        print('TURN GD0 OFF')
-        GD0.value(not GD0.value()) # toggle
+    PINOFF1 = request.find('/?Open-Close=OFF0')
+    if PINOFF1 == 6:
+        print('TURN PIN1 OFF')
+        PIN1.value(not PIN1.value()) # toggle
         time.sleep(1)
     response = html
     conn.send(response)
